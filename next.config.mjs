9a1b2ed6f982f87bad030fc92b7c123773095ecd
SyedHashir-Helper <syedhashir1001@gmail.com>
2024-11-30 12:07:18 +0500

    Remove Static Export

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    assetPrefix: './',
-    images: {
-        unoptimized: true, // Disable image optimization for static exports
-      },
 };
 
 export default nextConfig;
